c175acefd5c1c2b1bf8e7220bd6fdaab61e3b61b
theekshanagunathilake2003-png <theekshanagunathilake2003@gmail.com>
2026-06-11 15:33:52 +0530

    Add files via upload

---
+++ LW5/src/Q5/Cat.java
@@ -0,0 +1,14 @@
+package Q5;
+
+public class Cat extends Pet {
+
+    private String coatColor;
+
+    public void setCoatColor(String c) {
+        coatColor = c;
+    }
+
+    public String getCoatColor() {
+        return coatColor;
+    }
+}
---
+++ LW5/src/Q5/Dog.java
@@ -0,0 +1,14 @@
+package Q5;
+
+public class Dog extends Pet {
+
+    private double weight;
+
+    public void setWeight(double w) {
+        weight = w;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+}
